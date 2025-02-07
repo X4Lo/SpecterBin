15998f129a34a2483d89a19daae718de63d3a0f1
Mohammed Abad <test52308@gmail.com>
2025-02-07 17:17:05 +0100

    add paste manager

---
+++ src/services/pastesManager.ts
@@ -0,0 +1,77 @@
+import { PasteObject } from "@/types/PasteObject";
+import { EncryptionService } from "./encryptionService";
+import { pastesService } from "./pastesService";
+
+export class PastesManager {
+  static async createPaste(pasteObject: PasteObject) {
+    // Encode Content
+    if (pasteObject.isPasswordProtected && pasteObject.password) {
+      const dataObject = {
+        title: pasteObject.title,
+        syntaxHighlightingStyle: pasteObject.syntaxHighlightingStyle,
+        content: pasteObject.content,
+      };
+
+      const dataString = JSON.stringify(dataObject);
+
+      const encryptedData = await EncryptionService.encrypt(
+        dataString,
+        pasteObject.password
+      );
+
+      pasteObject.encryptedData = encryptedData;
+      pasteObject.title = undefined;
+      pasteObject.syntaxHighlightingStyle = undefined;
+      pasteObject.content = undefined;
+      pasteObject.password = undefined;
+    }
+
+    return pastesService.createPaste(pasteObject);
+  }
+
+  static async readPaste(
+    pasteObject: PasteObject
+  ): Promise<PasteObject | undefined> {
+    const currentDate = new Date();
+    // has already been read
+    if (pasteObject.hasBeenDecrypted) {
+      return undefined;
+    }
+
+    // expired
+    if (pasteObject.burnAfterDate && pasteObject.burnAfterDate < currentDate) {
+      pasteObject.encryptedData = "";
+      pasteObject.title = undefined;
+      pasteObject.syntaxHighlightingStyle = undefined;
+      pasteObject.content = undefined;
+      return undefined;
+    }
+
+    // no password given
+    if (pasteObject.isPasswordProtected && !pasteObject.password) {
+      return undefined;
+    }
+
+    if (
+      pasteObject.isPasswordProtected &&
+      pasteObject.password &&
+      pasteObject.encryptedData
+    ) {
+      const decryptedString = await EncryptionService.decrypt(
+        pasteObject.encryptedData,
+        pasteObject.password
+      );
+
+      const dataObject = JSON.parse(decryptedString);
+
+      if (dataObject.title) pasteObject.title = dataObject.title;
+      if (dataObject.syntaxHighlightingStyle)
+        pasteObject.syntaxHighlightingStyle =
+          dataObject.syntaxHighlightingStyle;
+      if (dataObject.content) pasteObject.content = dataObject.content;
+      pasteObject.hasBeenDecrypted = true;
+    }
+
+    return pasteObject;
+  }
+}
