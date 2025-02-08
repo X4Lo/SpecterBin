bf34ebbff176759c63d032256996484ce97110e6
X4Lo <test52308@gmail.com>
2025-02-08 18:43:34 +0100

    implemented app.js

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/database");
+const accountRoutes = require("./routes/account.routes");
+const pasteRoutes = require("./routes/paste.routes");
+
+const app = express();
+connectDB();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
+
+app.use(express.json());
+app.use("/accounts", accountRoutes);
+app.use("/pastes", pasteRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
