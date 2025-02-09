c1ed04cf2a41466a4ed1974afe8972ff3e61fe3c
X4Lo <test52308@gmail.com>
2025-02-09 11:02:14 +0100

    F: created account service

---
+++ frontend/src/services/accountService.ts
@@ -0,0 +1,14 @@
+import apiClient from "@/services/apiClient";
+import { Account } from "@/types/Account";
+
+export const accountService = {
+  async getAccountByAccountNumber(accountNumber: string): Promise<Account> {
+    const response = await apiClient.get<Account>(`/accounts/${accountNumber}`);
+    return response.data;
+  },
+
+  async createAccount(): Promise<Account> {
+    const response = await apiClient.post<Account>("/accounts/", {});
+    return response.data;
+  },
+};
